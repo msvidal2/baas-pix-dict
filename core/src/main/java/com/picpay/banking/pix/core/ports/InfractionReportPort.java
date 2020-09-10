c6eddf82485b10c87a7b9d76807c50a99bdb9d44
henrique.costa <Man@1643>
2020-09-10 15:10:10 -0300

    infraction base

---
+++ core/src/main/java/com/picpay/banking/pix/core/ports/InfractionReportPort.java
@@ -0,0 +1,10 @@
+package com.picpay.banking.pix.core.ports;
+
+
+import com.picpay.banking.pix.core.domain.Infraction;
+
+public interface InfractionReportPort {
+
+    Infraction execute(Infraction infraction, String requestIdentifier);
+
+}
