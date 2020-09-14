1a6a6ea6cef05d1f2ae7182ac63f0cfe552a612f
Diego Araujo <diego.araujo@picpay.com>
2020-09-14 15:54:37 -0300

    padronização de testes

--- application/src/test/java/com/picpay/banking/pix/adapters/incoming/web/PixKeyControllerTest.java
+++ application/src/test/java/com/picpay/banking/pix/adapters/incoming/web/PixKeyControllerTest.java
@@ -1,5 +1,6 @@
 package com.picpay.banking.pix.adapters.incoming.web;
 
+import com.picpay.banking.pix.adapters.incoming.web.dto.CreatePixKeyRequestWebDTO;
 import com.picpay.banking.pix.adapters.incoming.web.dto.UpdateAccountPixKeyDTO;
 import com.picpay.banking.pix.core.domain.*;
 import com.picpay.banking.pix.core.usecase.FindPixKeyUseCase;
@@ -12,11 +13,16 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDateTime;
 
 import static com.picpay.banking.pix.adapters.incoming.web.helper.ObjectMapperHelper.OBJECT_MAPPER;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
@@ -27,6 +33,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(MockitoExtension.class)
 public class PixKeyControllerTest {
+
+    public static final String BASE_URL = "/v1/keys";
 
     private MockMvc mockMvc;
 
@@ -71,7 +79,7 @@ public class PixKeyControllerTest {
         when(updateAccountUseCase.update(any(), any(), anyString())).thenReturn(pixKey);
         when(findPixKeyUseCase.findPixKeyUseCase(any(), anyString())).thenReturn(pixKey);
 
-        mockMvc.perform(put("/v1/keys/joao@picpay")
+        mockMvc.perform(put(BASE_URL +"/joao@picpay")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(OBJECT_MAPPER.asJsonString(UpdateAccountPixKeyDTO.builder()
                         .ispb(1)
@@ -90,6 +98,246 @@ public class PixKeyControllerTest {
                 .andExpect(jsonPath("$.nameIspb", equalTo("Empresa Picpay")))
                 .andExpect(jsonPath("$.accountType", equalTo("SALARY")))
                 .andExpect(jsonPath("$.personType", equalTo("INDIVIDUAL_PERSON")));
+    }
+
+    @Test
+    public void when_createPixKeyWithNullType_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setKey("teste@teste.com");
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountNumber("123456");
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setCpfCnpj("31254398713");
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithNullKey_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountNumber("123456");
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setCpfCnpj("31254398713");
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithNullIspb_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setKey("teste@teste.com");
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountNumber("123456");
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setCpfCnpj("31254398713");
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithNullAccountType_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setKey("teste@teste.com");
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountNumber("123456");
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setCpfCnpj("31254398713");
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithNullAccountNumber_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setKey("teste@teste.com");
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setCpfCnpj("31254398713");
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithNullAccountOpeningDate_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setKey("teste@teste.com");
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountNumber("123456");
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setCpfCnpj("31254398713");
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithNullPersonType_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setKey("teste@teste.com");
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountNumber("123456");
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setCpfCnpj("31254398713");
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithNullCpfCnpj_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setKey("teste@teste.com");
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountNumber("123456");
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithNullName_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setKey("teste@teste.com");
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountNumber("123456");
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setCpfCnpj("31254398713");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithNullReason_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setKey("teste@teste.com");
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountNumber("123456");
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setCpfCnpj("31254398713");
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithNullRequestIdentifier_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setKey("teste@teste.com");
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountNumber("123456");
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setCpfCnpj("31254398713");
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+
+        assertController(dto, status().is4xxClientError());
+    }
+
+    @Test
+    public void when_createPixKeyWithSuccess_expect_NullPointer() throws Exception {
+        CreatePixKeyRequestWebDTO dto = new CreatePixKeyRequestWebDTO();
+        dto.setType(KeyType.EMAIL);
+        dto.setKey("teste@teste.com");
+        dto.setIspb(12345);
+        dto.setBranchNumber("0001");
+        dto.setAccountType(AccountType.SALARY);
+        dto.setAccountNumber("123456");
+        dto.setAccountOpeningDate(LocalDateTime.now());
+        dto.setPersonType(PersonType.INDIVIDUAL_PERSON);
+        dto.setCpfCnpj("31254398713");
+        dto.setName("Silva Silva");
+        dto.setFantasyName("Fantasy Name");
+        dto.setReason(CreateReason.CUSTOMER_REQUEST);
+        dto.setRequestIdentifier("1234");
+
+        assertController(dto, status().isCreated());
+    }
+
+    private void assertController(CreatePixKeyRequestWebDTO dto, ResultMatcher match) throws Exception {
+        ResultActions resultActions = mockMvc.perform(post(BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(OBJECT_MAPPER.asJsonString(dto)))
+                .andDo(print())
+                .andExpect(match);
+
+        MvcResult result = resultActions.andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
     }
 
 }
