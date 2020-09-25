c5ebb0c5531dcd1f55b6a2b423978be28ca552c6
Diego Araujo <diego.araujo@picpay.com>
2020-09-25 10:10:48 -0300

    removida as dependencias para o projeto do original

--- application/src/main/java/com/picpay/banking/pix/adapters/incoming/web/ClientErrorResponseFactory.java
+++ application/src/main/java/com/picpay/banking/pix/adapters/incoming/web/ClientErrorResponseFactory.java
@@ -1,11 +1,8 @@
 package com.picpay.banking.pix.adapters.incoming.web;
 
-import com.netflix.hystrix.exception.HystrixRuntimeException;
 import com.picpay.banking.jdpi.dto.response.JDErrorDTO;
 import com.picpay.banking.jdpi.exception.JDClientException;
 import com.picpay.banking.pix.adapters.incoming.web.dto.ErrorDTO;
-import com.picpay.banking.pix.original.dto.response.OriginalErrorDTO;
-import com.picpay.banking.pix.original.exception.OriginalClientException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,23 +18,11 @@ public class ClientErrorResponseFactory {
         if (clientException.getClass().isAssignableFrom(JDClientException.class)) {
             return getJDErrorResponse((JDClientException) clientException);
 
-        } else if (clientException.getClass().isAssignableFrom(OriginalClientException.class)) {
-            return getOriginalErrorResponse((OriginalClientException) clientException);
         }
 
         return ResponseEntity
                 .status(INTERNAL_SERVER_ERROR)
                 .body(ErrorDTO.from(INTERNAL_SERVER_ERROR, clientException.getMessage()));
-    }
-
-    private static ResponseEntity<ErrorDTO> getOriginalErrorResponse(OriginalClientException clientException) {
-        var message = clientException.getError()
-                .orElse(new OriginalErrorDTO("", "", Collections.emptyList()))
-                .getError();
-
-        return ResponseEntity
-                .status(clientException.getStatus())
-                .body(ErrorDTO.from(clientException.getStatus(), message));
     }
 
     private static ResponseEntity<ErrorDTO> getJDErrorResponse(JDClientException clientException) {
