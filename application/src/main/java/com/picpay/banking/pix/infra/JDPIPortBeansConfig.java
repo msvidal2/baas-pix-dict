06ec37ef72e105c66340ae390af09ac1d5fb6827
Diego Araujo <diego.araujo@picpay.com>
2020-10-02 22:15:37 -0300

    remove hystrix fallback from TokenManagerClient and add resilience4j circuit breaker

--- application/src/main/java/com/picpay/banking/pix/infra/JDPIPortBeansConfig.java
+++ application/src/main/java/com/picpay/banking/pix/infra/JDPIPortBeansConfig.java
@@ -4,40 +4,15 @@ import com.picpay.banking.jdpi.clients.ClaimJDClient;
 import com.picpay.banking.jdpi.clients.InfractionReportJDClient;
 import com.picpay.banking.jdpi.clients.PixKeyJDClient;
 import com.picpay.banking.jdpi.clients.TokenManagerClient;
-import com.picpay.banking.jdpi.converter.CreateClaimConverter;
-import com.picpay.banking.jdpi.converter.CreatePixKeyConverter;
-import com.picpay.banking.jdpi.converter.FindPixKeyConverter;
-import com.picpay.banking.jdpi.converter.ListClaimConverter;
-import com.picpay.banking.jdpi.converter.ListPixKeyConverter;
+import com.picpay.banking.jdpi.converter.*;
 import com.picpay.banking.jdpi.interceptors.FeignClientInterceptor;
 import com.picpay.banking.jdpi.ports.TimeLimiterExecutor;
-import com.picpay.banking.jdpi.ports.claim.ClaimCancelPortImpl;
-import com.picpay.banking.jdpi.ports.claim.ClaimConfirmationPortImpl;
-import com.picpay.banking.jdpi.ports.claim.CompleteClaimPortImpl;
-import com.picpay.banking.jdpi.ports.claim.CreateClaimPortImpl;
-import com.picpay.banking.jdpi.ports.pixkey.CreatePixKeyPortImpl;
-import com.picpay.banking.jdpi.ports.claim.FindClaimPortImpl;
-import com.picpay.banking.jdpi.ports.pixkey.FindPixKeyPortImpl;
+import com.picpay.banking.jdpi.ports.claim.*;
 import com.picpay.banking.jdpi.ports.infraction.InfractionReportPortImpl;
-import com.picpay.banking.jdpi.ports.claim.ListClaimPortImpl;
-import com.picpay.banking.jdpi.ports.claim.ListPendingClaimPortImpl;
-import com.picpay.banking.jdpi.ports.pixkey.ListPixKeyPortImpl;
-import com.picpay.banking.jdpi.ports.pixkey.RemovePixKeyPortImpl;
-import com.picpay.banking.jdpi.ports.pixkey.UpdateAccountPixKeyPortImpl;
-import com.picpay.banking.pix.core.ports.claim.ClaimCancelPort;
-import com.picpay.banking.pix.core.ports.claim.ClaimConfirmationPort;
-import com.picpay.banking.pix.core.ports.claim.CompleteClaimPort;
-import com.picpay.banking.pix.core.ports.claim.CreateClaimPort;
-import com.picpay.banking.pix.core.ports.pixkey.CreatePixKeyPort;
-import com.picpay.banking.pix.core.ports.claim.FindClaimPort;
-import com.picpay.banking.pix.core.ports.pixkey.FindPixKeyPort;
+import com.picpay.banking.jdpi.ports.pixkey.*;
+import com.picpay.banking.pix.core.ports.claim.*;
 import com.picpay.banking.pix.core.ports.infraction.InfractionReportPort;
-import com.picpay.banking.pix.core.ports.claim.ListClaimPort;
-import com.picpay.banking.pix.core.ports.claim.ListPendingClaimPort;
-import com.picpay.banking.pix.core.ports.pixkey.ListPixKeyPort;
-import com.picpay.banking.pix.core.ports.pixkey.RemovePixKeyPort;
-import com.picpay.banking.pix.core.ports.pixkey.UpdateAccountPixKeyPort;
-import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
+import com.picpay.banking.pix.core.ports.pixkey.*;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -47,8 +22,9 @@ import org.springframework.context.annotation.Configuration;
 public class JDPIPortBeansConfig {
 
     @Bean
-    public FeignClientInterceptor feignClientInterceptor(TokenManagerClient tokenManagerClient){
-        return new FeignClientInterceptor(tokenManagerClient);
+    public FeignClientInterceptor feignClientInterceptor(TokenManagerClient tokenManagerClient,
+                                                         TimeLimiterExecutor timeLimiterExecutor) {
+        return new FeignClientInterceptor(tokenManagerClient, timeLimiterExecutor);
     }
 
     @Bean
