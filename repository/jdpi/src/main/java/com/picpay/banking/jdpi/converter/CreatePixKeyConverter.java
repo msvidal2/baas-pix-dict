56e7ab01c0150ac40f1c892bd00e018e8f2169ac
Diego Araujo <diego.araujo@picpay.com>
2020-10-03 18:58:20 -0300

    correção de criação de chave random, não estava retornando a chave

--- repository/jdpi/src/main/java/com/picpay/banking/jdpi/converter/CreatePixKeyConverter.java
+++ repository/jdpi/src/main/java/com/picpay/banking/jdpi/converter/CreatePixKeyConverter.java
@@ -3,6 +3,7 @@ package com.picpay.banking.jdpi.converter;
 import com.picpay.banking.jdpi.dto.request.CreatePixKeyRequestDTO;
 import com.picpay.banking.jdpi.dto.response.CreatePixKeyResponseJDDTO;
 import com.picpay.banking.pix.core.domain.CreateReason;
+import com.picpay.banking.pix.core.domain.KeyType;
 import com.picpay.banking.pix.core.domain.PixKey;
 import org.springframework.stereotype.Component;
 
@@ -11,38 +12,38 @@ public class CreatePixKeyConverter {
 
     public CreatePixKeyRequestDTO convert(final PixKey from, final CreateReason reason) {
         return CreatePixKeyRequestDTO.builder()
-                .tpChave(           from.getType().getValue())
-                .chave(             from.getKey())
-                .ispb(              from.getIspb())
-                .nrAgencia(         from.getBranchNumber())
-                .tpConta(           from.getAccountType().getValue())
-                .nrConta(           from.getAccountNumber())
-                .dtHrAberturaConta( from.getAccountOpeningDate())
-                .tpPessoa(          from.getPersonType().getValue())
-                .cpfCnpj(           Long.valueOf(from.getTaxId()))
-                .nome(              from.getName())
-                .nomeFantasia(      from.getFantasyName())
-                .motivo(            reason.getValue())
+                .tpChave(from.getType().getValue())
+                .chave(from.getKey())
+                .ispb(from.getIspb())
+                .nrAgencia(from.getBranchNumber())
+                .tpConta(from.getAccountType().getValue())
+                .nrConta(from.getAccountNumber())
+                .dtHrAberturaConta(from.getAccountOpeningDate())
+                .tpPessoa(from.getPersonType().getValue())
+                .cpfCnpj(Long.valueOf(from.getTaxId()))
+                .nome(from.getName())
+                .nomeFantasia(from.getFantasyName())
+                .motivo(reason.getValue())
                 .build();
     }
 
     public PixKey convert(final CreatePixKeyResponseJDDTO createPixKeyResponseJDDTO,
-                                 final PixKey from) {
+                          final PixKey from) {
         return PixKey.builder()
-                .type(              from.getType())
-                .key(               from.getKey())
-                .ispb(              from.getIspb())
-                .branchNumber(      from.getBranchNumber())
-                .accountType(       from.getAccountType())
-                .accountNumber(     from.getAccountNumber())
+                .type(from.getType())
+                .key(KeyType.RANDOM.equals(from.getType()) ? createPixKeyResponseJDDTO.getChave() : from.getKey())
+                .ispb(from.getIspb())
+                .branchNumber(from.getBranchNumber())
+                .accountType(from.getAccountType())
+                .accountNumber(from.getAccountNumber())
                 .accountOpeningDate(from.getAccountOpeningDate())
-                .personType(        from.getPersonType())
-                .taxId(             from.getTaxId())
-                .name(              from.getName())
-                .fantasyName(       from.getFantasyName())
+                .personType(from.getPersonType())
+                .taxId(from.getTaxId())
+                .name(from.getName())
+                .fantasyName(from.getFantasyName())
                 .accountOpeningDate(from.getAccountOpeningDate())
-                .createdAt(         createPixKeyResponseJDDTO.getDtHrCriacaoChave())
-                .startPossessionAt( createPixKeyResponseJDDTO.getDtHrInicioPosseChave())
+                .createdAt(createPixKeyResponseJDDTO.getDtHrCriacaoChave())
+                .startPossessionAt(createPixKeyResponseJDDTO.getDtHrInicioPosseChave())
 
                 .build();
     }
