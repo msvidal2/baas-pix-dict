ec38fe7f9b5f54ae73e07736fa8d23c71bc73482
Diego Araujo <diego.araujo@picpay.com>
2020-11-18 14:21:02 -0300

    xmlElement no CreateEntryResponse

--- infra/src/main/java/com/picpay/banking/pixkey/dto/response/CreateEntryResponse.java
+++ infra/src/main/java/com/picpay/banking/pixkey/dto/response/CreateEntryResponse.java
@@ -17,6 +17,7 @@ import lombok.NoArgsConstructor;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -31,6 +32,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CreateEntryResponse {
 
+    @XmlElement(name = "Entry")
     private Entry entry;
 
 }
