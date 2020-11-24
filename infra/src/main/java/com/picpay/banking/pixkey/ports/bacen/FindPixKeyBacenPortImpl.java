2fb3bb95202cfaf018180301e0a2426a5ad67708
Diego Araujo <diego.araujo@picpay.com>
2020-11-24 15:42:43 -0300

    hash PayerId

--- infra/src/main/java/com/picpay/banking/pixkey/ports/bacen/FindPixKeyBacenPortImpl.java
+++ infra/src/main/java/com/picpay/banking/pixkey/ports/bacen/FindPixKeyBacenPortImpl.java
@@ -7,10 +7,16 @@ import com.picpay.banking.pix.core.domain.PixKey;
 import com.picpay.banking.pix.core.ports.pixkey.bacen.FindPixKeyBacenPort;
 import com.picpay.banking.pixkey.clients.BacenKeyClient;
 import com.picpay.banking.pixkey.dto.response.GetEntryResponse;
+import com.picpay.banking.util.Encrypt;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import static net.logstash.logback.argument.StructuredArguments.kv;
 
@@ -21,21 +27,29 @@ import static net.logstash.logback.argument.StructuredArguments.kv;
  * @version 1.0 18/11/20
  */
 @Slf4j
-@RequiredArgsConstructor
 @Component
 public class FindPixKeyBacenPortImpl implements FindPixKeyBacenPort {
 
     private static final String CIRCUIT_BREAKER_NAME = "find-pix-key-bacen";
 
+    private final String participant;
+
     private final BacenKeyClient bacenKeyClient;
+
+    public FindPixKeyBacenPortImpl(@Value("${picpay.ispb}") String participant,
+                                   BacenKeyClient bacenKeyClient) {
+        this.participant = participant;
+        this.bacenKeyClient = bacenKeyClient;
+    }
 
     @Override
     @CircuitBreaker(name = CIRCUIT_BREAKER_NAME, fallbackMethod = "fallbackMethod")
     public PixKey findPixKey(String requestIdentifier, String pixKey, String userId) {
+
+        var payerId = Encrypt.sha256(participant + userId);
+
         //FIXME Remover código fixo
-        GetEntryResponse getEntryResponse = bacenKeyClient.findPixKey(
-                "22896431",
-                "9117e65bc9a6a1ed724f2302287f7aa6a8fcff72cb44fb6a51e667e2d523e517",
+        GetEntryResponse getEntryResponse = bacenKeyClient.findPixKey(participant, payerId,
                 "E2289643120201014221500000000001",
                 pixKey);
         //TODO Ao buscar no Bacen a informação, ela deverá ser incluída no DB?
