588cbd443139fea3f8892745067d71a6c6f5042c
Diego Araujo <diego.araujo@picpay.com>
2020-11-26 10:41:08 -0300

    listkeys specification

--- infra/src/main/java/com/picpay/banking/pixkey/repository/PixKeyRepository.java
+++ infra/src/main/java/com/picpay/banking/pixkey/repository/PixKeyRepository.java
@@ -10,14 +10,16 @@ import com.picpay.banking.pix.core.domain.PersonType;
 import com.picpay.banking.pixkey.entity.PixKeyEntity;
 import com.picpay.banking.pixkey.entity.PixKeyIdEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PixKeyRepository extends JpaRepository<PixKeyEntity, PixKeyIdEntity> {
+public interface PixKeyRepository extends JpaRepository<PixKeyEntity, PixKeyIdEntity>, JpaSpecificationExecutor<PixKeyEntity> {
 
     Optional<PixKeyEntity> findByIdKey(String key);
 
@@ -27,22 +29,5 @@ public interface PixKeyRepository extends JpaRepository<PixKeyEntity, PixKeyIdEn
             "   AND t.accountNumber = :accountNumber " +
             "   AND t.accountType = :accountType")
     List<PixKeyEntity> findByAccount(Integer participant, String branch, String accountNumber, AccountType accountType);
-
-    @Query("SELECT t FROM pix_key t " +
-            "WHERE t.id.taxId = :taxId " +
-            "   AND t.personType = :personType " +
-            "   AND t.branch = :branch " +
-            "   AND t.accountNumber = :accountNumber " +
-            "   AND t.accountType = :accountType " +
-            "   AND t.participant = :participant")
-    List<PixKeyEntity> findKeys(String taxId, PersonType personType, String branch, String accountNumber, AccountType accountType, Integer participant);
-
-    @Query("SELECT t FROM pix_key t " +
-            "WHERE t.id.taxId = :taxId " +
-            "   AND t.personType = :personType " +
-            "   AND t.accountNumber = :accountNumber " +
-            "   AND t.accountType = :accountType " +
-            "   AND t.participant = :participant")
-    List<PixKeyEntity> findKeys(String taxId, PersonType personType, String accountNumber, AccountType accountType, Integer participant);
 
 }
