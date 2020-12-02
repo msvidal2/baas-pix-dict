ee25aa88b8d034509f483abf6c300509241f58df
Diego Araujo <diego.araujo@picpay.com>
2020-12-02 17:22:57 -0300

    adicionado modulo de pulling

---
+++ bacen-pulling/src/main/java/com/picpay/banking/pix/dict/BaasPixDictBacenPullingApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.banking.pix.dict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaasPixDictBacenPullingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaasPixDictBacenPullingApplication.class, args);
+	}
+
+}
