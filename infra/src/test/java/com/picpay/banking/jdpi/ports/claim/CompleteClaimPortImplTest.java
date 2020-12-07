5e3a7b1310f9253d2d7bceb3563483f2725d3f4d
araujojannayna <jannayna.araujo@picpay.com>
2020-12-07 09:30:40 -0300

    Claim - Adicionando alteracao de data no confirm e corrigindo testes do complete

--- infra/src/test/java/com/picpay/banking/jdpi/ports/claim/CompleteClaimPortImplTest.java
+++ infra/src/test/java/com/picpay/banking/jdpi/ports/claim/CompleteClaimPortImplTest.java
@@ -1,10 +1,17 @@
 package com.picpay.banking.jdpi.ports.claim;
 
+import com.picpay.banking.claim.dto.ClaimReason;
+import com.picpay.banking.claim.dto.request.ClaimType;
+import com.picpay.banking.claim.dto.response.ClaimResponse;
+import com.picpay.banking.claim.dto.response.ClaimStatus;
+import com.picpay.banking.claim.dto.response.CompleteClaimResponse;
 import com.picpay.banking.claim.ports.bacen.CompleteClaimPortBacenImpl;
-import com.picpay.banking.jdpi.dto.response.ClaimResponseDTO;
 import com.picpay.banking.jdpi.ports.TimeLimiterExecutor;
-import com.picpay.banking.pix.core.domain.Claim;
-import com.picpay.banking.pix.core.domain.ClaimSituation;
+import com.picpay.banking.pix.core.domain.*;
+import com.picpay.banking.pixkey.dto.request.*;
+import com.picpay.banking.pixkey.dto.request.AccountType;
+import com.picpay.banking.pixkey.dto.request.PersonType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -29,17 +36,49 @@ public class CompleteClaimPortImplTest {
     @Mock
     private TimeLimiterExecutor timeLimiterExecutor;
 
-    // TODO - corrigir teste
-    //@Test
-    void testComplete() {
-        var claimId = randomUUID().toString();
+    private ClaimResponse claim;
 
-        var clientResponse = ClaimResponseDTO.builder()
-                .claimId(claimId)
-                .claimSituation(2)
-                .resolutionThresholdDate(LocalDateTime.now())
-                .completionThresholdDate(LocalDateTime.now())
-                .lastModifiedDate(LocalDateTime.now())
+    private String claimId = randomUUID().toString();
+
+    @BeforeEach
+    void setup(){
+        Account claimerAccount = Account.builder()
+                .participant("12345")
+                .branch("12345")
+                .accountNumber("12345")
+                .accountType(AccountType.CACC)
+                .openingDate(LocalDateTime.now())
+                .build();
+
+        Owner claimer = Owner.builder()
+                .taxIdNumber("12345")
+                .name("Jose Joaquim")
+                .type(PersonType.NATURAL_PERSON)
+                .build();
+
+        claim = ClaimResponse.builder()
+                .id(claimId)
+                .type(ClaimType.PORTABILITY)
+                .key("+5584999999999")
+                .keyType(KeyTypeBacen.PHONE)
+                .claimerAccount(claimerAccount)
+                .claimer(claimer)
+                .donorParticipant("6789")
+                .status(ClaimStatus.COMPLETED)
+                .completionPeriodEnd(LocalDateTime.now())
+                .resolutionPeriodEnd(LocalDateTime.now())
+                .lastModified(LocalDateTime.now())
+                .cancelReason(null)
+                .confirmReason(ClaimReason.USER_REQUESTED)
+                .cancelledBy(null)
+                .build();
+
+    }
+
+    @Test
+    void testComplete() {
+        var clientResponse = CompleteClaimResponse.builder()
+                .claim(claim)
                 .build();
 
         when(timeLimiterExecutor.execute(anyString(), any(), anyString())).thenReturn(clientResponse);
@@ -53,10 +92,10 @@ public class CompleteClaimPortImplTest {
                     randomUUID().toString());
 
             assertEquals(claimId, claimResponse.getClaimId());
-            assertEquals(ClaimSituation.CONFIRMED, claimResponse.getClaimSituation());
-            assertEquals(clientResponse.getResolutionThresholdDate(), claimResponse.getResolutionThresholdDate());
-            assertEquals(clientResponse.getCompletionThresholdDate(), claimResponse.getCompletionThresholdDate());
-            assertEquals(clientResponse.getLastModifiedDate(), claimResponse.getLastModifiedDate());
+            assertEquals(ClaimSituation.COMPLETED, claimResponse.getClaimSituation());
+            assertEquals(clientResponse.getClaim().getResolutionPeriodEnd(), claimResponse.getResolutionThresholdDate());
+            assertEquals(clientResponse.getClaim().getCompletionPeriodEnd(), claimResponse.getCompletionThresholdDate());
+            assertEquals(clientResponse.getClaim().getLastModified(), claimResponse.getLastModifiedDate());
         });
     }
 
