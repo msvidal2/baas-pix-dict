690c556b71fb38d799097bab16f28d402734c057
Rafael Braga <rafael.braga@picpay.com>
2020-12-10 18:19:53 -0300

    Ajustes no task

--- polling/infraction/src/main/java/com/picpay/banking/InfractionPollingTaskApplication.java
+++ polling/infraction/src/main/java/com/picpay/banking/InfractionPollingTaskApplication.java
@@ -1,18 +1,20 @@
 package com.picpay.banking;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.cloud.task.configuration.EnableTask;
 
 @EnableFeignClients(basePackages = "com.picpay.banking.infraction.clients")
 @SpringBootApplication(scanBasePackages = {"com.picpay.banking.config", "com.picpay.banking.infraction.*"})
-public class BaasPixDictBacenInfractionTaskApplication {
+@EnableTask
+public class InfractionPollingTaskApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext =
-            SpringApplication.run(BaasPixDictBacenInfractionTaskApplication.class, args);
-        System.exit(SpringApplication.exit(applicationContext));
+        new SpringApplicationBuilder(InfractionPollingTaskApplication.class)
+            .web(WebApplicationType.NONE)
+            .run(args);
     }
 
 }
