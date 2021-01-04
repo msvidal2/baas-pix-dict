e3614c99e05bfe896c6fcf64f1b7d5e7fdcb0f5a
Luis Ricardo Lisboa da Silva <picpay@ip-10-210-128-139.ec2.internal>
2021-01-04 15:35:55 -0300

    Removendo Parametro Key

--- sync/cid-files/src/main/java/com/picpay/banking/pix/sync/file/task/SyncFileTask.java
+++ sync/cid-files/src/main/java/com/picpay/banking/pix/sync/file/task/SyncFileTask.java
@@ -7,6 +7,9 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
+import java.util.Arrays;
+
 /**
  * @author Luis Silva
  * @version 1.0 10/12/2020
@@ -15,16 +18,17 @@ import org.springframework.stereotype.Component;
 public class SyncFileTask implements ApplicationRunner {
 
     private FailureReconciliationSyncByFileUseCase failureReconciliationSyncByFileUseCase;
-    private KeyType keyType;
 
-    public SyncFileTask(final FailureReconciliationSyncByFileUseCase failureReconciliationSyncByFileUseCase, @Value("${sync.key-type}") final KeyType keyType) {
+    public SyncFileTask(final FailureReconciliationSyncByFileUseCase failureReconciliationSyncByFileUseCase) {
         this.failureReconciliationSyncByFileUseCase = failureReconciliationSyncByFileUseCase;
-        this.keyType = keyType;
     }
 
     @Override
+    @Transactional
     public void run(final ApplicationArguments args) throws Exception {
-        this.failureReconciliationSyncByFileUseCase.execute(keyType);
+        Arrays.stream(KeyType.values()).forEach(keyType -> {
+            this.failureReconciliationSyncByFileUseCase.execute(keyType);
+        });
     }
 
 }
