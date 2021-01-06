f56a997aec59177a2b092f5ab11fe51646b034c3
Diego Araujo <diego.araujo@picpay.com>
2021-01-06 11:19:53 -0300

    ajuste no claim pooling

--- polling/claim/src/main/java/com/picpay/banking/claim/config/ClaimBindingConfig.java
+++ polling/claim/src/main/java/com/picpay/banking/claim/config/ClaimBindingConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 
 @EnableBinding(value = {
         ClaimTopicBindingOutput.class,
-        OverduePossessionClaimOutputTopic.class,
         ClaimNotificationOutputBinding.class
 })
 @Configuration
