3c01e07628877e4b75867fc186d67b1d710d54ec
Rodrigo Pizzi Argentato <rodrigo.argentato@picpay.com>
2021-02-12 13:58:56 -0300

    Alterado sincronismo por eventos para: 1. Rodar uma scheduler de tempos em tempos para recuperar os eventos de cids do bacen e gravar em uma nova tabela no banco de dados 2. Quando é necessário reconciliação por eventos, foi alterado para ler os dados desta nova tabela, ao invés do ir até o bacen e recuparar todos os eventos
    Esta alteração tem um impacto muito forte em performance quando há necessidade de correção de dados por eventos, pois elimina o passo de ir até o bacen e aguardar o retorno de um volume grande de informações

--- sync/sync-verifier/src/main/java/com/picpay/banking/SyncVerifierApplication.java
+++ sync/sync-verifier/src/main/java/com/picpay/banking/SyncVerifierApplication.java
@@ -3,14 +3,15 @@ package com.picpay.banking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableFeignClients(basePackages = "com.picpay.banking.reconciliation.clients")
 @SpringBootApplication(scanBasePackages = { "com.picpay.banking.common.*", "com.picpay.banking.config", "com.picpay.banking.reconciliation.*", "com.picpay.banking.pixkey.*"})
 public class SyncVerifierApplication {
 
     public static void main(String[] args) {
-        var ctx = SpringApplication.run(SyncVerifierApplication.class, args);
-        System.exit(SpringApplication.exit(ctx));
+        SpringApplication.run(SyncVerifierApplication.class, args);
     }
 
 }
--- sync/sync-verifier/src/main/java/com/picpay/banking/reconciliation/service/SincronizeCidEventsScheduler.java
+++ sync/sync-verifier/src/main/java/com/picpay/banking/reconciliation/service/SincronizeCidEventsScheduler.java
@@ -4,6 +4,7 @@ import com.newrelic.api.agent.Trace;
 import com.picpay.banking.pix.core.domain.KeyType;
 import com.picpay.banking.pix.core.usecase.reconciliation.SincronizeCIDEventsUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +14,11 @@ import java.util.Arrays;
 @RequiredArgsConstructor
 public class SincronizeCidEventsScheduler {
 
+    private static final String EVERY_HOUR = "0 0 0/1 1/1 * ?";
     private final SincronizeCIDEventsUseCase sincronizeCIDEventsUseCase;
 
     @Transactional
+    @Scheduled(cron = EVERY_HOUR)
     @Trace(dispatcher = true, metricName = "sincronizeCidEventsScheduler_all")
     public void run() {
         Arrays.stream(KeyType.values()).forEach(sincronizeCIDEventsUseCase::syncByKeyType);
