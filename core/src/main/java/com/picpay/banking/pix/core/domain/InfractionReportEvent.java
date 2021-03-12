b85b43cbfd57784524671c04efb5703bfa0cbc20
Diego Araujo <diego.araujo@picpay.com>
2021-03-12 15:45:12 -0300

    entities para a tabela de eventos de infração

---
+++ core/src/main/java/com/picpay/banking/pix/core/domain/InfractionReportEvent.java
@@ -0,0 +1,12 @@
+package com.picpay.banking.pix.core.domain;
+
+public enum InfractionReportEvent {
+
+    PENDING_CREATE,
+    PENDING_UPDATE,
+    PENDING_REMOVE,
+    CREATED,
+    REMOVED,
+    UPDATED;
+
+}
---
+++ infra/database/src/main/java/com/picpay/banking/infraction/entity/InfractionReportEventEntity.java
@@ -0,0 +1,44 @@
+package com.picpay.banking.infraction.entity;
+
+import com.picpay.banking.pix.core.domain.InfractionReportEvent;
+import com.picpay.banking.pix.core.domain.infraction.InfractionReport;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity(name = "pix_key_event")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class InfractionReportEventEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator="native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private Long id;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private InfractionReportEvent type;
+
+    private String infractionReportId;
+
+    @Type(type = "json")
+    @Column(name = "event_data", columnDefinition = "json", nullable = false)
+    private InfractionReport data;
+
+    @CreatedDate
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    private String requestIdentifier;
+
+}
