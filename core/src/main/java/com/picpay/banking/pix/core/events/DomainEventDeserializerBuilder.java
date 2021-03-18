5e22acc7329f85a831e618c31f2669e8e94fd620
Diego Araujo <diego.araujo@picpay.com>
2021-03-18 14:15:56 -0300

    deserializador do DomainEvent

---
+++ core/src/main/java/com/picpay/banking/pix/core/events/DomainEventDeserializerBuilder.java
@@ -0,0 +1,65 @@
+package com.picpay.banking.pix.core.events;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.picpay.banking.pix.core.events.data.ErrorEvent;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@JsonPOJOBuilder(buildMethodName = "build", withPrefix = "")
+public class DomainEventDeserializerBuilder {
+
+    private EventType eventType;
+    private Domain domain;
+    private Object source;
+    private ErrorEvent errorEvent;
+    private String requestIdentifier;
+
+    public DomainEventDeserializerBuilder eventType(EventType eventType) {
+        this.eventType = eventType;
+        return this;
+    }
+
+    public DomainEventDeserializerBuilder domain(Domain domain) {
+        this.domain = domain;
+        return this;
+    }
+
+    public DomainEventDeserializerBuilder source(Object source) {
+        this.source = source;
+        return this;
+    }
+
+    public DomainEventDeserializerBuilder errorEvent(ErrorEvent errorEvent) {
+        this.errorEvent = errorEvent;
+        return this;
+    }
+
+    public DomainEventDeserializerBuilder requestIdentifier(String requestIdentifier) {
+        this.requestIdentifier = requestIdentifier;
+        return this;
+    }
+
+    public DomainEvent build() {
+        var dateModule = new SimpleModule();
+        dateModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        dateModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ISO_LOCAL_DATE));
+
+        var mapper = new ObjectMapper();
+        mapper.registerModules(dateModule);
+
+        return DomainEvent.builder()
+                .eventType(eventType)
+                .domain(domain)
+                .source(mapper.convertValue(source, eventType.getAClass()))
+                .errorEvent(errorEvent)
+                .requestIdentifier(requestIdentifier)
+                .build();
+    }
+
+}
